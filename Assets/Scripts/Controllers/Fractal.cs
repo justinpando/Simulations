012ece50382ca80da204e41ea88ff5ae94c47101
Justin Pando <justin.pando@gmail.com>
2014-10-22 17:08:36 -0700

    planar cube fractals

--- Assets/Scripts/Controllers/Fractal.cs
+++ Assets/Scripts/Controllers/Fractal.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class Fractal : MonoBehaviour {
 
+	public Mesh mesh;
+	public Material material;
+	
+	public int maxDepth;
+	private int depth;	
+	public float childScale;
+	
 	// Use this for initialization
 	void Start () {
-	
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;	
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
 	}
 	
+	private void Initialize (Fractal parent, Vector3 direction) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		
+		childScale = parent.childScale;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+	}
+		
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	private IEnumerator CreateChildren () {
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up);
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right);		
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.left);	
 	}
 }
